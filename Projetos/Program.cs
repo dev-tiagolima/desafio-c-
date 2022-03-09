6847ffd23ccbbf74691d78b6342575806eea6832
Tiago <klayveth@gmail.com>
2022-03-09 12:38:42 -0300

    commit final

--- Projetos/Program.cs
+++ Projetos/Program.cs
@@ -1,24 +1,53 @@
 ﻿using System;
-using Projetos.Entities;
+
 
 namespace Projetos
 {
 
-  class Program{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            int n, nota, quociente, resto, n100 = 0, n50 = 0, n20 = 0, n10 = 0, n5 = 0, n2 = 0, n1 = 1;
+           
 
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
 
-    static void Main(string[] args)
-      {
-      Hero arus = new Hero("Arudo", "knight", 1234, 555, 333);
-      Wizard jef = new Wizard("Jef", "white-wizard", 1259, 440, 980);
-    
-      Console.WriteLine(jef);
-      Console.WriteLine(jef.Attack());
+            resto = n;
+
+            nota = 100;
+            quociente = resto / 100;
+            resto = resto % 100;
+
+        n100 = n / 100;
+        n %= 100;
+        n50 = n / 50;
+        n %= 50;
+        n20 = n / 20;
+        n %= 20;
+        n10 = n / 10;
+        n %= 10;
+        n5 = n / 5;
+        n %= 5;
+        n2 = n / 2;
+        n %= 2;
+        n1 = n;
+
+        Console.WriteLine(resto);
+        Console.WriteLine($"{n100} nota(s) de R$ 100,00");
+        Console.WriteLine($"{n50} nota(s) de R$ 50,00");
+        Console.WriteLine($"{n20} nota(s) de R$ 20,00");
+        Console.WriteLine($"{n10} nota(s) de R$ 10,00");
+        Console.WriteLine($"{n5} nota(s) de R$ 5,00");
+        Console.WriteLine($"{n2} nota(s) de R$ 2,00");
+        Console.WriteLine($"{n1} nota(s) de R$ 1,00");
+
+return 0;
+         
+
+    }                                       
+        }
+    }
       
-      
-
-      }
-
-            }
-
-}
+  
